704569a1912d7f1bf0360cf5c07d7aa2ff3482f1
Varathalakshmi <varathaathara@gmail.com>
2017-11-16 19:42:37 +0530

    UI

---
+++ app/src/main/java/com/edinbridge/autojobs/autojobshere/Hotjob_descriptionActivity.java
@@ -0,0 +1,13 @@
+package com.edinbridge.autojobs.autojobshere;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Hotjob_descriptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hotjob_description);
+    }
+}
